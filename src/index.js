01db42b25b31e32e1205ef50d1c78361c4482c61
Apoorv Mishra <apoorvmishra101092@gmail.com>
2020-12-13 00:33:32 +0530

    Add signup and login

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Redirect,
+  Link,
 } from "react-router-dom";
-
 
 /* Structure of TodoApp */
 // <TodoApp>
@@ -226,7 +226,15 @@ class TodoList extends React.Component {
 
 function Header(props) {
   return (
-    <div className="header">Todos</div>
+    <div>
+      <div className="d-flex flex-row bd-highlight mb-3 justify-content-between">
+	<div className="p-2 bd-highlight">Welcome, {props.name}</div>
+	<div className="p-2 bd-highlight">
+	  <button type="button" onClick={props.logout} className="btn btn-primary">Logout</button>
+	</div>
+      </div>   
+      <div className="header">Todos</div>
+    </div>
   );
 }
 
@@ -296,7 +304,6 @@ class Home extends React.Component {
     this.state = {
       email: '',
       password: '',
-      isLoggedIn: false
     }
   }
 
@@ -304,7 +311,6 @@ class Home extends React.Component {
     this.setState({
       email: e.target.value,
       password: this.state.password,
-      isLoggedIn: this.state.isLoggedIn
     })
   }
 
@@ -312,25 +318,52 @@ class Home extends React.Component {
     this.setState({
       email: this.state.email,
       password: e.target.value,
-      isLoggedIn: this.state.isLoggedIn
     });
   }
 
-  handleLogin(e) {
+  async handleLogin(e) {
     e.preventDefault();
-    this.setState({
+
+    // TODO:validate
+    // Utils.validateEmail(this.state.firstName)
+    // Utils.validatePassword(this.state.firstName)
+
+    async function postData(url = '', data = {}) {
+      // Default options are marked with *
+      const response = await fetch(url, {
+	method: 'POST', // *GET, POST, PUT, DELETE, etc.
+	mode: 'cors', // no-cors, *cors, same-origin
+	cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+	credentials: 'same-origin', // include, *same-origin, omit
+	headers: {
+	  'Content-Type': 'application/json'
+	  // 'Content-Type': 'application/x-www-form-urlencoded',
+	},
+	redirect: 'follow', // manual, *follow, error
+	referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+	body: JSON.stringify(data) // body data type must match "Content-Type" header
+      });
+      return response.json(); // parses JSON response into native JavaScript objects
+    }
+
+    const data = await postData('http://localhost:8080/login', {
       email: this.state.email,
-      password: this.state.password,
-      isLoggedIn: true
+      password: this.state.password 
     });
+
+    if (data.message === 'Login successful!') {
+      localStorage.setItem('user', JSON.stringify({ token: data.token, id: data.id, name: data.name }));
+      this.props.toggleUserLoginStatus(true);
+    }
   }
 
   render() {
 
-    if (this.state.isLoggedIn) {
+    if (this.props.isUserLoggedIn) {
+      const name = JSON.parse(localStorage.getItem('user')).name;
       return (
 	<div>
-	  <Header />
+	  <Header name={name} logout={this.props.logout}/>
 	  <TodoList />
 	</div>
       );
@@ -361,27 +394,114 @@ class SignUp extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: ''
+    }
+
+    this.handleSignUp = this.handleSignUp.bind(this);
+    this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
+    this.handleLastNameChange = this.handleLastNameChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
   }
 
+  handleFirstNameChange(e) {
+    this.setState({
+      firstName: e.target.value,
+      lastName: this.state.lastName,
+      email: this.state.email,
+      password: this.state.password
+    });
+  }
+
+  handleLastNameChange(e) {
+    this.setState({
+      firstName: this.state.firstName,
+      lastName: e.target.value,
+      email: this.state.email,
+      password: this.state.password
+    });
+  }
+
+  handleEmailChange(e) {
+    this.setState({
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      email: e.target.value,
+      password: this.state.password
+    });
+  }
+
+  handlePasswordChange(e) {
+    this.setState({
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      email: this.state.email,
+      password: e.target.value
+    });
+  }
+
+  async handleSignUp(e) {
+    e.preventDefault();
+
+    // TODO:validate
+    // Utils.validateName(this.state.firstName)
+    // Utils.validateName(this.state.firstName)
+    // Utils.validateEmail(this.state.email)
+    // Utils.validatePassword(this.state.email)
+
+    async function postData(url = '', data = {}) {
+      // Default options are marked with *
+      const response = await fetch(url, {
+	method: 'POST', // *GET, POST, PUT, DELETE, etc.
+	mode: 'cors', // no-cors, *cors, same-origin
+	cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+	credentials: 'same-origin', // include, *same-origin, omit
+	headers: {
+	  'Content-Type': 'application/json'
+	  // 'Content-Type': 'application/x-www-form-urlencoded',
+	},
+	redirect: 'follow', // manual, *follow, error
+	referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+	body: JSON.stringify(data) // body data type must match "Content-Type" header
+      });
+      return response.json(); // parses JSON response into native JavaScript objects
+    }
+
+    const data = await postData('http://localhost:8080/signup', {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      email: this.state.email,
+      password: this.state.password 
+    });
+
+    if (data.message === 'SignUp successful!') {
+      localStorage.setItem('user', JSON.stringify({ token: data.token, id: data.id, name: data.name }));
+      this.props.toggleUserLoginStatus(true);
+    }
+  }
 
   render() {
     return (
       <div>
-	<form>
+	<form onSubmit={this.handleSignUp}>
 	  <div className="form-group">
 	    <label htmlFor="exampleFirstName1">First name</label>
-	    <input type="text" className="form-control" id="exampleFirstName1" aria-describedby="firstNameHelp" placeholder="First name"/>
+	    <input type="text" value={this.state.firstName} onChange={this.handleFirstNameChange} className="form-control" id="exampleFirstName1" aria-describedby="firstNameHelp" placeholder="First name"/>
 	  </div>
 	  <div className="form-group">
 	    <label htmlFor="exampleLastName1">Last name</label>
-	    <input type="text" className="form-control" id="exampleLastName1" placeholder="Last name"/>
+	    <input type="text" value={this.state.lastName} onChange={this.handleLastNameChange} className="form-control" id="exampleLastName1" placeholder="Last name"/>
 	  </div>
 	  <div className="form-group"> <label htmlFor="exampleInputEmail1">Email address</label>
-	    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+	    <input type="email" value={this.state.email} onChange={this.handleEmailChange} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
 	  </div>
 	  <div className="form-group">
 	    <label htmlFor="exampleInputPassword1">Password</label>
-	    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+	    <input type="password" value={this.state.password} onChange={this.handlePasswordChange} className="form-control" id="exampleInputPassword1" placeholder="Password"/>
 	  </div>
 	  <button type="submit" className="btn btn-primary">Submit</button>
 	</form>
@@ -391,19 +511,44 @@ class SignUp extends React.Component {
 
 }
 
-function TodoApp(props) {
-  return (
-    <Router>
-      <Switch>
-	<Route exact path="/">
-	  <Home />
-	</Route>
-	<Route path="/signup">
-	  <SignUp />
-	</Route>
-      </Switch>
-    </Router>
-  );
+class TodoApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isUserLoggedIn: localStorage.getItem('user') ? true : false
+    };
+
+    this.toggleUserLoginStatus = this.toggleUserLoginStatus.bind(this);
+    this.logout = this.logout.bind(this);
+  }
+
+  toggleUserLoginStatus(isLoggedIn) {
+    this.setState({
+      isUserLoggedIn: isLoggedIn
+    });
+  }
+
+  logout() {
+    localStorage.clear();
+    this.setState({
+      isUserLoggedIn: false
+    });
+  }
+
+  render() {
+    return (
+      <Router>
+	<Switch>
+	  <Route exact path="/">
+	    <Home isUserLoggedIn={this.state.isUserLoggedIn} logout={this.logout} toggleUserLoginStatus={(s) => this.toggleUserLoginStatus(s)} />
+	  </Route>
+	  <Route path="/signup">
+	    {this.state.isUserLoggedIn ? <Redirect to='/' /> : <SignUp toggleUserLoginStatus={(status) => this.toggleUserLoginStatus(status)} />}
+	  </Route>
+	</Switch>
+      </Router>
+    );
+  }
 }
 
 ReactDOM.render(
