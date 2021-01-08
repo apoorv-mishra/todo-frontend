de10138ef7e74af5986397b1532c7daaa8413838
Apoorv Mishra <apoorvmishra101092@gmail.com>
2021-01-08 16:58:25 +0530

    Use hooks instead of class based components

--- src/components/TodoList/Footer/Footer.js
+++ src/components/TodoList/Footer/Footer.js
@@ -1,56 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class Footer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      active: 0
-    };
+function Footer(props) {
+  const [active, setActive] = useState(0);
+
+  function onClick(id) {
+    setActive(id);
   }
 
-  onClick(id) {
-    this.setState({
-      active: id
-    });
-  }
+  const getClasses = id => (id === active) ? "filter-buttons selected" : "filter-buttons";
 
-  render() {
-    const getClasses = id => (id === this.state.active) ? "filter-buttons selected" : "filter-buttons";
-
-    return (
-      <div className="footer">
-	<div className="undone-item-count-container">
-	  <p className="undone-item-count">{this.props.activeTodosCount} items left</p>
-	</div>
-	<div className="filter-buttons-container">
-	  <button
-	    className={getClasses(0)}
-	    onClick={() => {
-	      this.onClick(0);
-	      this.props.displayAllTodos();
-	    }}>
-	    All
-	  </button>
-	  <button
-	    className={getClasses(1)}
-	    onClick={() => {
-	      this.onClick(1);
-	      this.props.displayActiveTodos();
-	    }}>
-	    Active
-	  </button>
-	  <button
-	    className={getClasses(2)}
-	    onClick={() => {
-	      this.onClick(2);
-	      this.props.displayCompletedTodos();
-	    }}>
-	    Completed
-	  </button>
-	</div>
+  return (
+    <div className="footer">
+      <div className="undone-item-count-container">
+	<p className="undone-item-count">{props.activeTodosCount} items left</p>
       </div>
-    );
-  }
+      <div className="filter-buttons-container">
+	<button
+	  className={getClasses(0)}
+	  onClick={() => {
+	    onClick(0);
+	    props.displayAllTodos();
+	  }}>
+	  All
+	</button>
+	<button
+	  className={getClasses(1)}
+	  onClick={() => {
+	    onClick(1);
+	    props.displayActiveTodos();
+	  }}>
+	  Active
+	</button>
+	<button
+	  className={getClasses(2)}
+	  onClick={() => {
+	    onClick(2);
+	    props.displayCompletedTodos();
+	  }}>
+	  Completed
+	</button>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
